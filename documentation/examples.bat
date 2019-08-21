edf2808a6888fc0aee2895506523e6cc44e8b74e
Michael Hofmann BE <Michael.Hofmann@OrangeObjects.de>
2019-08-21 14:18:49 +0200

    Adjustments (names, packages,..)

--- documentation/examples.bat
+++
@@ -1,8 +0,0 @@
-
-
-create pmwiki for MobileHwServices
--v net.michaelhofmann.usecasefull.visitor.PmWikiNodeExecutor -f C:\proj\wob\abis\AbisClient\sysdoc\useCases\AbisDataDicUseCases.xml -of C:\proj\wob\abis\AbisClient\sysdoc\useCases\AbisDataDicUseCases.txt
-
-
-create markdown for Bias2LDAP project
--v net.michaelhofmann.usecasefull.visitor.StandardMarkdownExecutor -f C:\proj\wob\biasinternetgroupservice\doku\BiasUseCases.xml -td C:\proj\gitmisc\usecasefull\templates\markdown -of C:\proj\wob\biasipgroupwebapi\doku\freyja\output.md
